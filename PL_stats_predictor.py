ce2a86450c95401d7385a1aafe4f5c40076e4b07
Svyatoslav-source <Svyatoslav.Aspidov@city.ac.uk>
2026-02-01 13:36:33 +0000

    Update on load_data and get_rolling_stats functions

--- PL_stats_predictor.py
+++ PL_stats_predictor.py
@@ -1,31 +1,48 @@
 import pandas as pd
-import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
+from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
 
 
 def load_data(path):
     # Read the data from the csv file
     df = pd.read_csv(path)
-    return df
+    df['Date'] = pd.to_datetime(df['Date'], dayfirst=True)
+    return df.sort_values('Date')
     
 
-def process_stats(df):
-    print("Columns available to us: ", df.columns.tolist())
-    return df
+def get_rolling_stats(df, n_games=5):
+    # Standardize data to track every team's history
+    h = df[['Date', 'HomeTeam', 'FTHG', 'FTAG', 'FTR', 'HST']].copy()
+    a = df[['Date', 'AwayTeam', 'FTAG', 'FTHG', 'FTR', 'AST']].copy()
 
-def train_and_predict(features, targets):
-    pass
+    cols = ['Date', 'Team', 'GoalsFor', 'GoalsAgainst', 'Result', 'SOT']
+    h.columns, a.columns = cols, cols
 
-def plot_likelihood(results):
-    pass
+    # Calculate points for each game 
+    def pts(res, is_home):
+        if res == 'D': return 1
+        if (res == 'H' and is_home) or (res=='A' and not is_home): return 3
+        return 0
+    
+    h['Points'] = h['Result'].apply(lambda x: pts(x, True))
+    a['Points'] = a['Result'].apply(lambda x: pts(x, False))
+
+    all_games = pd.concat([h, a]).sort_values(['Team', 'Date'])
+            
+
+    # Calculate Rolling averages (The .shift(1) is used to prevent data leakage)
+    for col in ['GoalsFor', 'GoalsAgainst', 'Points', 'SOT']:
+        all_games[f'Rolling_{col}'] = all_games.groupby('Team')[col].transform(lambda x: x.rolling(n_games, closed='left').mean())
+        return all_games    
+    
 
 
 
 if __name__ == "__main__":
     print("Model Training Intialized...")
     path = r"C:\Users\svyat\Downloads\E0 (1).csv"
-    df = load_data(path)
-
-    process_stats(df)
+    
 
